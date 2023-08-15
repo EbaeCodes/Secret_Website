233fafbb80d46911a5b15957d77241f8c2dabde7
Eberechukwu Nwaobiora <ebere.nwaobiora@gmail.com>
2023-08-15 02:50:21 +0200

    Cookies added

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
 //jshint esversion:6
 require("dotenv").config();
-const md5 = require("md5");
+//const md5 = require("md5");
+//const bcrypt = require('bcrypt');
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption")
 const express = require("express");
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
 const salt = 10;
 const ejs = require("ejs");
 const app = express();
-
-// Replace the uri string with your connection string.
-mongoose.connect("mongodb://localhost:27017/secretDB",{useNewUrlParser:true});
+const session = require('express-session')
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose")
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));// to save our css files
 
+//cookies
+app.use(session({
+  secret: 'SecretInSecret',
+  resave: false,
+  saveUninitialized: false,
+  //cookie: { secure: true }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Replace the uri string with your connection string.
+mongoose.connect("mongodb://localhost:27017/secretDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
     email : String,
     password: String
  });
+ userSchema.plugin(passportLocalMongoose);
  userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]})
 
 
  //the model
  const User = mongoose.model("User", userSchema);
+
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
  app.get("/",function(request,response){
@@ -46,31 +67,40 @@ const userSchema = new mongoose.Schema({
     response.render("login");
  });
 
- app.post("/register",function(request,response){
-    bcrypt.hash(request.body.password, salt, async(err, hash)=> {
-        // Store hash in your password DB.
-         const newUser = new User({
-            email:request.body.username,
-            password: hash
-          });
-          await newUser.save().then(()=> response.render("secrets"));
-    }) 
+ app.get("/secrets",function(request,response){
+   if(request.isAuthenticated()){
+    response.render("secrets");
+   }else{
+    response.redirect("/");
+   }
  });
 
- -
- app.post("/login",async(request,response)=>{
-      const email = request.body.username;
-      const password = request.body.password;
-      const found = await User.findOne({email:email});
-
-      bcrypt.compare(password, found.password, function(err, result) {
-        if(result === true){
-            response.render("secrets");
+ app.post("/register",function(request,response){
+    User.register({username:request.body.username},request.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            response.redirect("/register");
         }else{
-            var errorMessage = "Incorrect username or password";
-            // response.render("login", {error:errorMessage});
-            console.log(errorMessage)
-        }  
+            passport.authenticate("local")(request,response,function(){
+            response.redirect("/secrets");
+            });
+        }
+    })
+ });
+
+ app.post("/login",async(request,response)=>{
+   const user = new User({
+    username: request.body.username,
+    password: request.body.password
+   })
+    request.login(user,function(err){
+        if(err){
+            console.log(err)
+        }else{
+            passport.authenticate("local")(request,response,function(){
+                response.redirect("/secrets");
+            });
+        }
     });
 });
 
