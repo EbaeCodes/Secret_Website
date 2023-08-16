a7150c6b0d3bb8c10c192f688343720751afb7a2
Eberechukwu Nwaobiora <ebere.nwaobiora@gmail.com>
2023-08-16 12:18:21 +0200

    Sign in with facebook and google configured

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const app = express();
 const session = require('express-session')
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose")
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -33,10 +36,13 @@ mongoose.connect("mongodb://localhost:27017/secretDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
     email : String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
  });
  userSchema.plugin(passportLocalMongoose);
  userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]})
+ userSchema.plugin(findOrCreate);
 
 
  //the model
@@ -46,13 +52,66 @@ const userSchema = new mongoose.Schema({
 passport.use(User.createStrategy());
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
  app.get("/",function(request,response){
     response.render("home");
  });
+
+ app.get("/auth/google",
+    passport.authenticate("google", { scope: ['profile'] }));
+ 
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(request,response) {
+      // Successful authentication, redirect home.
+      response.redirect('/secrets');
+    });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets', 
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(request,response) {
+      // Successful authentication, redirect home.
+      response.redirect('/secrets');
+    });
+
 
  app.get("/register",function(request,response){
     response.render("register");
@@ -67,12 +126,21 @@ passport.deserializeUser(User.deserializeUser());
     response.render("login");
  });
 
- app.get("/secrets",function(request,response){
-   if(request.isAuthenticated()){
-    response.render("secrets");
-   }else{
-    response.redirect("/");
-   }
+
+ app.get("/submit",function(request,response){
+    if(request.isAuthenticated()){
+        response.render("submit");
+       }else{
+        response.redirect("/login");
+       }
+ });
+
+
+ app.get("/secrets",async(request,response)=>{
+    const allFound = await User.find({"secret": {$ne:null}});
+    if(allFound){
+        response.render("secrets",{userWithSecrets:allFound})
+    }
  });
 
  app.post("/register",function(request,response){
@@ -103,6 +171,22 @@ passport.deserializeUser(User.deserializeUser());
         }
     });
 });
+
+app.post("/submit",async(request,response) => {
+    const secretSubmit = request.body.secret;
+    //Find the current user of the app
+     const ID = request.user.id;
+     console.log(ID);
+     const found = await User.findById({ID});
+   
+     if(found){
+        found.secret = secretSubmit;
+        await found.save();
+        response.redirect("/secrets"); // to see their secrets
+     }else{
+        response.render("error"); // to see their secrets
+     }
+ });
 
 
 
